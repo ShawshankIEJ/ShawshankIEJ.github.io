76bd238256caae00b90428485f517210f11fb17a
ShawshankIEJ <3207142375@qq.com>
2025-11-13 16:21:27 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,13 @@
 <script lang="ts" setup>
 // 页脚需要的链接
 const links = {
-    Nerfies: "https://github.com/nerfies/nerfies.github.io",
-    Vue: "https://cn.vuejs.org/",
-    Vite: "https://cn.vitejs.dev/",
-    ElementPlus: "https://element-plus.org/zh-CN/",
-    template: "https://github.com/JunyaoHu/academic-project-page-template-vue",
+     Nerfies: "",
+     Vue: "",
+     Vite: "",
+     ElementPlus: "",
+     template: "",
 }
 </script>
-
-<template>
-  
-  <div class="bg">
-    
-    <el-watermark :content="['JunyaoHu', 'Academic Project', 'Page Template']" :gap="['60','0']" :z-index="0">
-      <el-row justify="center">
-        <p class="footer">
-          This template is inspired by 
-          <a :href="links.Nerfies">Nerfies</a>,<br/>
-          
-          powered by 
-          <a :href="links.Vue">Vue</a>, 
-          <a :href="links.Vite">Vite</a>, and
-          <a :href="links.ElementPlus">Element Plus</a>.<br/>
-          
-          You can fork🛠️ / star✨/ PR📃 from <a :href="links.template">here</a>.
-        </p>
-      </el-row>
-    </el-watermark>
-  </div>
-</template>
 
 <style scoped>
 
