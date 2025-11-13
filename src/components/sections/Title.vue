5287744a09dd1d563acaf4909b2c8f71a1a67140
ShawshankIEJ <3207142375@qq.com>
2025-11-13 16:19:13 +0800

    Update Title.vue

--- src/components/sections/Title.vue
+++ src/components/sections/Title.vue
@@ -4,16 +4,16 @@ import { ElIcon } from 'element-plus'
 import { Document, Files, MagicStick, Picture, DataAnalysis, Film } from '@element-plus/icons-vue'
 
 // logo地址，没有则置为""即可
-const logo = './logo.png'
+const logo = ''
 
 // 标题
-const title = 'Academic Project Page Template'
+const title = 'The Shawshank Redemption of Embodied AI: Understanding and Benchmarking Indirect Environmental Jailbreaks'
 
 // 标题颜色
 const title_color = '#000000'
 
 // 标题补充，没有则置为''即可
-const title_supp = ' (Vue based)'
+const title_supp = ''
 
 // 标题补充颜色
 const title_supp_color = '#42B883'
@@ -23,104 +23,31 @@ const btn_color = '#444444'
 
 // 作者清单（包含作者姓名、头像、主页、地址序号）
 const authors = [
-  {
-    name: "Your Name",
-    icon: "./icon/junyaohu.jpg",
-    homepage: "https://junyaohu.github.io/",
-    address_flag: "1,#"
-  },
-  {
-    name: "Anya Forger",
-    icon: "./icon/anya.jpg",
-    homepage: "https://www.bilibili.com/video/BV1jv4y1P7Bb",
-    address_flag: "2,#"
-  },
-  {
-    name: "BugCat Capoo",
-    icon: "./icon/capoo.webp",
-    homepage: "https://zh.moegirl.org.cn/%E7%8C%AB%E7%8C%AB%E8%99%AB%E5%92%96%E6%B3%A2",
-    address_flag: "1,*"
-  },
+  // {
+  //   name: "Your Name",
+  //   icon: "",
+  //   homepage: "",
+  //   address_flag: "1,#"
+  // },
 ]
 
 // 地址清单（包含地址名称、头像、主页、地址序号）
-const addresses = [
-  {
-    address_flag: "1",
-    name: "Home University",
-    icon: "./icon/home.png",
-    homepage: "https://github.com/hmuniversity"
-  },
-  {
-    address_flag: "2",
-    name: "IKUN University",
-    icon: "./icon/ikun.avif",
-    homepage: "https://www.bilibili.com/video/BV178411Y7QB"
-  },
-]
+const addresses = []
 
 // 共一和通讯提示
-const con_and_corresponding_author = 
-  "#: Equal Contribution. *: Corresponding Author."
+const con_and_corresponding_author = ""
+  // "#: Equal Contribution. *: Corresponding Author."
 
 // 最新消息
-const news = "🔥 [2024-12-15] This template project is still under development."
+const news = ""
 
 // 强调内容
 const emphases = [
-  "🎉 [ABCD 2024] Poster",
-  "🥰 欢迎关注“减论”微信公众号/B站/知乎/小红书",
-  "传递人工智能算法科普教育的减约理解",
-  "提升信息效率及认知维度"
+  "Warning: This paper contains potentially harmful AI-generated language and aggressive actions!"
 ]
 
 // 提供引导资料链接
-const buttons = [
-  {
-    disabled: true,
-    name: "Paper",
-    component: Document,
-  },
-  {
-    disabled: true,
-    name: "中译版",
-    component: Document,
-  },
-  {
-    disabled: false,
-    name: "Code",
-    link: "https://github.com/JunyaoHu/academic-project-page-template-vue",
-    component: Files,
-  },
-  {
-    disabled: false,
-    name: "Demo",
-    link: "https://junyaohu.github.io/academic-project-page-template-vue",
-    component: MagicStick,
-  },
-  {
-    disabled: true,
-    name: "Poster",
-    component: Picture,
-  },
-  {
-    disabled: true,
-    name: "Slide",
-    component: DataAnalysis,
-  },
-  {
-    disabled: false,
-    name: "Video (减论)",
-    link: "https://www.bilibili.com/video/BV15XkgYiE73/",
-    component: Film,
-  },
-  {
-    disabled: false,
-    name: "Video (Tutorial)",
-    link: "https://www.bilibili.com/video/BV1oUrfYzEqZ",
-    component: Film,
-  },
-]
+const buttons = []
 
 </script>
 
@@ -150,7 +77,7 @@ const buttons = [
     </el-row>
 
     <!-- 作者名单 -->
-    <el-row justify="center">
+    <!--<el-row justify="center">
       <a :href=author.homepage v-for="author in authors">
         <el-button class="title-button" type="primary" text>
           <el-avatar v-if="author.icon" :size="40" :src="author.icon" />
@@ -159,10 +86,10 @@ const buttons = [
           </span>
         </el-button>
       </a>
-    </el-row>
+    </el-row> -->
 
     <!-- 地址名单 -->
-    <el-row justify="center">
+    <!-- <el-row justify="center">
       <a :href=address.homepage v-for="address in addresses">
         <el-button class="title-button" type="primary" text>
           <el-avatar v-if="address.icon" :size="40" :src="address.icon" />
@@ -171,20 +98,20 @@ const buttons = [
           </span>
         </el-button>
       </a>
-    </el-row>
+    </el-row> -->
 
     <!-- 共一和通讯提示内容 -->
-    <el-row justify="center" class="con-cor">
+    <!--  <el-row justify="center" class="con-cor">
         {{ con_and_corresponding_author }}
-    </el-row>
+    </el-row>  -->
 
     <!-- 强调内容 -->
-    <el-row justify="center" class="emphasis" v-for="emphasis in emphases">
+    <!-- <el-row justify="center" class="emphasis" v-for="emphasis in emphases">
         {{ emphasis }}
-    </el-row>
+    </el-row>  -->
 
     <!-- 提供引导按钮 -->
-    <el-row justify="center" style="margin-bottom: 20px;">
+    <!-- <el-row justify="center" style="margin-bottom: 20px;">
       <el-col :span="20">
         <el-row justify="center">
           <a :href=button.link v-for="button in buttons">
@@ -197,7 +124,7 @@ const buttons = [
           </a>
         </el-row>
       </el-col>
-    </el-row>
+    </el-row> -->
 
   </div>
 </template>
@@ -214,61 +141,61 @@ const buttons = [
 }
 
 /* 姓名和地址按钮 */
-.title-button {
+/* .title-button {
   margin: 10px 3px;
-}
+} */
 
 /* 姓名和地址按钮光标悬浮 */
-.title-button:hover {
+/* .title-button:hover {
   margin: 10px 8px;
-}
+} */
 
 /* 引导材料按钮 */
-.guidance-button {
+/* .guidance-button {
   margin: 8px 5px;
   box-shadow: #d8d8d8 1px 1px 1px 1px;
-}
+} */
 
 /* 姓名属性 */
-.author {
+/* .author {
   font-size: 18px;
   margin-left: 3px;
-}
+} */
 
 /* 姓名上标属性 */
-.name_sup {
+/* .name_sup {
   color: #606266; 
   margin-left: 3px;
-}
+} */
 
 /* 地址属性 */
-.address {
+/* .address {
   font-size: 18px;
-}
+} */
 
 /* 地址上标属性 */
-.address_sup {
+/* .address_sup {
   color: #606266; 
   margin-right: 1px;
-}
+} */
 
 /* 头像属性 */
-.el-avatar {
+/* .el-avatar {
   margin-right: 6px;
   box-shadow: #b7b7b7 0px 0px 3px 1px;
-}
+} */
 
 /* 共一和通讯文字属性 */
-.con-cor {
+/* .con-cor {
   font-family: Arial;
   font-size: 14px;
   margin: 18px 0px;
   text-align: center;
-}
+} */
 
 /* 强调信息属性 */
 .emphasis {
-  color: chocolate;
+  color: red;
   font-weight: bold;
   margin: 8px;
   font-size: 22px;
@@ -289,13 +216,13 @@ const buttons = [
   margin: 0;
 }
 
-.logo {
+/* .logo {
   width: 150px; 
   height: 150px;
   border-radius: 50%;
   box-shadow: #ced3dc 0px 0px 3px 2px;
   margin-top: 40px;
-}
+} */
 
 /* 手机端链接样式处理 */
 a:-webkit-any-link {
