be6b39377c149abb2563c9cc8392f84a3dd237e5
ShawshankIEJ <3207142375@qq.com>
2026-05-12 09:41:02 +0800

    Update Evaluation.vue

--- src/components/sections/Evaluation.vue
+++ src/components/sections/Evaluation.vue
@@ -14,6 +14,8 @@ const tableData1 = [
   { model: 'ICA', dc_asr: 0.07, dc_hrs: 0.66, di_asr: 0.31, di_hrs: 1.89, wr_asr: 0.17, wr_hrs: 1.14, sd_asr: 0.03, sd_hrs: 0.49, ip_asr: 0.09, ip_hrs: 1.04, overall_asr: 0.17, overall_hrs: 1.18 },
   { model: 'MJP', dc_asr: 0.08, dc_hrs: 0.87, di_asr: 0.19, di_hrs: 1.36, wr_asr: 0.17, wr_hrs: 1.32, sd_asr: 0.07, sd_hrs: 0.77, ip_asr: 0.07, ip_hrs: 0.86, overall_asr: 0.12, overall_hrs: 1.08 },
   { model: 'Multilingual', dc_asr: 0.05, dc_hrs: 0.46, di_asr: 0.07, di_hrs: 0.57, wr_asr: 0.11, wr_hrs: 1.05, sd_asr: 0.04, sd_hrs: 0.58, ip_asr: 0.07, ip_hrs: 0.58, overall_asr: 0.06, overall_hrs: 0.55 },
+  { model: 'SceneTAP', dc_asr: 0.13, dc_hrs: 1.74, di_asr: 0.58, di_hrs: 4.27, wr_asr: 0.12, wr_hrs: 1.50, sd_asr: 0.11, sd_hrs: 1.33, ip_asr: 0.29, ip_hrs: 4.00, overall_asr: 0.34, overall_hrs: 2.98 },
+  { model: 'SceneTAP-Adapted', dc_asr: 0.24, dc_hrs: 2.76, di_asr: 0.74, di_hrs: 5.39, wr_asr: 0.33, wr_hrs: 4.00, sd_asr: 0.01, sd_hrs: 0.50, ip_asr: 0.17, ip_hrs: 4.67, overall_asr: 0.45, overall_hrs: 3.98},
   { model: 'Vanilla', dc_asr: 0.15, dc_hrs: 1.40, di_asr: 0.52, di_hrs: 3.49, wr_asr: 0.29, wr_hrs: 2.69, sd_asr: 0.11, sd_hrs: 1.21, ip_asr: 0.13, ip_hrs: 1.71, overall_asr: 0.29, overall_hrs: 2.27 },
   { model: 'Shawshank-Vanilla', dc_asr: 0.51, dc_hrs: 4.40, di_asr: 0.75, di_hrs: 5.41, wr_asr: 0.66, wr_hrs: 5.17, sd_asr: 0.39, sd_hrs: 3.61, ip_asr: 0.33, ip_hrs: 3.72, overall_asr: 0.57, overall_hrs: 4.64 },
   { model: 'Shawshank', dc_asr: 0.77, dc_hrs: 6.68, di_asr: 0.84, di_hrs: 6.19, wr_asr: 0.77, wr_hrs: 5.98, sd_asr: 0.58, sd_hrs: 5.35, ip_asr: 0.54, ip_hrs: 5.18, overall_asr: 0.75, overall_hrs: 6.13 },
@@ -21,8 +23,11 @@ const tableData1 = [
 
 const tableData2 = [
   { model: 'GPT-4o', dc_asr: 0.50, dc_hrs: 4.17, di_asr: 0.74, di_hrs: 5.02, wr_asr: 0.69, wr_hrs: 4.92, sd_asr: 0.44, sd_hrs: 3.82, ip_asr: 0.43, ip_hrs: 3.97, overall_asr: 0.59, overall_hrs: 4.47 },
+  { model: 'GPT-5.5', dc_asr: 0.42, dc_hrs: 4.23, di_asr: 0.62, di_hrs: 4.49, wr_asr: 0.25, wr_hrs: 1.75, sd_asr: 0.22, sd_hrs: 2.22, ip_asr: 0.57, ip_hrs: 5.00, overall_asr: 0.49, overall_hrs: 4.02 },
   { model: 'Qwen3-VL', dc_asr: 0.77, dc_hrs: 6.68, di_asr: 0.84, di_hrs: 6.19, wr_asr: 0.77, wr_hrs: 5.98, sd_asr: 0.58, sd_hrs: 5.35, ip_asr: 0.54, ip_hrs: 5.18, overall_asr: 0.75, overall_hrs: 6.13 },
+  { model: 'Qwen3-VL', dc_asr: 0.61, dc_hrs: 4.65, di_asr: 0.80, di_hrs: 6.51, wr_asr: 0.38, wr_hrs: 5.50, sd_asr: 0.22, sd_hrs: 4.44, ip_asr: 0.57, ip_hrs: 5.29, overall_asr: 0.64, overall_hrs: 5.58 },
   { model: 'Gemini-2.0', dc_asr: 0.62, dc_hrs: 5.42, di_asr: 0.69, di_hrs: 5.02, wr_asr: 0.32, wr_hrs: 3.13, sd_asr: 0.34, sd_hrs: 3.37, ip_asr: 0.24, ip_hrs: 3.01, overall_asr: 0.56, overall_hrs: 4.66 },
+  { model: 'Gemini-3.1', dc_asr: 0.39, dc_hrs: 2.81, di_asr: 0.56, di_hrs: 4.22, wr_asr: 0.12, wr_hrs: 1.62, sd_asr: 0.11, sd_hrs: 1.67, ip_asr: 0.14, ip_hrs: 4.14, overall_asr: 0.40, overall_hrs: 3.34 },
   { model: 'GLM-4.5', dc_asr: 0.28, dc_hrs: 3.62, di_asr: 0.35, di_hrs: 3.39, wr_asr: 0.37, wr_hrs: 4.85, sd_asr: 0.26, sd_hrs: 2.90, ip_asr: 0.18, ip_hrs: 2.77, overall_asr: 0.30, overall_hrs: 3.36 },
   { model: 'Claude-3.5', dc_asr: 0.15, dc_hrs: 1.99, di_asr: 0.28, di_hrs: 2.54, wr_asr: 0.22, wr_hrs: 2.86, sd_asr: 0.21, sd_hrs: 2.56, ip_asr: 0.17, ip_hrs: 2.44, overall_asr: 0.21, overall_hrs: 2.36 },
   { model: 'Deepseek-VL2', dc_asr: 0.32, dc_hrs: 3.32, di_asr: 0.34, di_hrs: 3.14, wr_asr: 0.37, wr_hrs: 3.43, sd_asr: 0.22, sd_hrs: 2.49, ip_asr: 0.30, ip_hrs: 3.26, overall_asr: 0.32, overall_hrs: 3.15 },
@@ -38,13 +43,13 @@ const tableData3 = [
 ]
 
 const tableData4 = [
-  { model: 'BadRobot-CJ', qwen3guard_asr: 0.26, qwen3guard_hrs: 4.00, sap_asr: 0.55, sap_hrs: 3.56 },
-  { model: 'RoboPAIR', qwen3guard_asr: 0.43, qwen3guard_hrs: 4.06, sap_asr: 0.57, sap_hrs: 3.89 },
-  { model: 'Cipher', qwen3guard_asr: 0.49, qwen3guard_hrs: 3.73, sap_asr: 0.45, sap_hrs: 3.23 },
-  { model: 'CodeChameleon', qwen3guard_asr: 0.26, qwen3guard_hrs: 3.09, sap_asr: 0.50, sap_hrs: 3.71 },
-  { model: 'ReNeLLM', qwen3guard_asr: 0.29, qwen3guard_hrs: 3.60, sap_asr: 0.35, sap_hrs: 3.13 },
-  { model: 'Shawshank-Vanilla', qwen3guard_asr: 0.18, qwen3guard_hrs: 2.21, sap_asr: 0.37, sap_hrs: 2.93 },
-  { model: 'Shawshank', qwen3guard_asr: 0.52, qwen3guard_hrs: 6.13, sap_asr: 0.65, sap_hrs: 4.51 }
+  { model: 'BadRobot-CJ', qwen3guard_asr: 0.26, qwen3guard_hrs: 0.55, sap_asr: 0.58, sap_hrs: 0.13 },
+  { model: 'RoboPAIR', qwen3guard_asr: 0.43, qwen3guard_hrs: 0.57, sap_asr: 0.57, sap_hrs: 0.10 },
+  { model: 'Cipher', qwen3guard_asr: 0.49, qwen3guard_hrs: 0.45, sap_asr: 0.16, sap_hrs: 0.44 },
+  { model: 'CodeChameleon', qwen3guard_asr: 0.26, qwen3guard_hrs: 0.50, sap_asr: 0.04, sap_hrs: 0.12 },
+  { model: 'ReNeLLM', qwen3guard_asr: 0.29, qwen3guard_hrs: 0.35, sap_asr: 0.16, sap_hrs: 0.13 },
+  { model: 'Shawshank-Vanilla', qwen3guard_asr: 0.18, qwen3guard_hrs: 0.37, sap_asr: 0.27, sap_hrs: 0.29 },
+  { model: 'Shawshank', qwen3guard_asr: 0.52, qwen3guard_hrs: 0.65, sap_asr: 0.59, sap_hrs: 0.52 }
 ]
 
 
@@ -117,10 +122,10 @@ const tableData4 = [
                     <el-tab-pane label="Defenses" name="Method D">
                         <el-table :data="tableData4" style="width: 100%">
                             <el-table-column fixed prop="model" label="Method"  sortable align="center"/>
-                            <el-table-column prop="qwen3guard_asr" label="Qwen3Guard ASR" sortable align="center"/>
-                            <el-table-column prop="qwen3guard_hrs" label="Qwen3Guard HRS" sortable align="center"/>
-                            <el-table-column prop="sap_asr" label="SAP ASR" sortable align="center"/>
-                            <el-table-column prop="sap_hrs" label="SAP HRS" sortable align="center"/>
+                            <el-table-column prop="qwen3guard_asr" label="Qwen3Guard" sortable align="center"/>
+                            <el-table-column prop="qwen3guard_hrs" label="SAP" sortable align="center"/>
+                            <el-table-column prop="sap_asr" label="Llama-Guard-4-12B" sortable align="center"/>
+                            <el-table-column prop="sap_hrs" label="WildGuard" sortable align="center"/>
                         </el-table>
                     </el-tab-pane>
                     </el-tabs>
